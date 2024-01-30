db8f308b763cedf1f085d9f5266f9b7f83bc2cd4
Addison Douglas <addisondouglas850@gmail.com>
2024-01-30 12:33:04 -0800

    Main

---
+++ Main.py
@@ -0,0 +1,15 @@
+# Import the Canvas class
+from canvasapi import Canvas
+
+# Canvas API URL
+API_URL = "https://canvas.uw.edu"
+# Canvas API key
+API_KEY = "10~2fyu2iLVjm1XSaW5uc8QAQf8voBcevOCDQOwQNsQOaCeUw27VpOGic01sNETQWbp"
+
+# Initialize a new Canvas object
+canvas = Canvas(API_URL, API_KEY)
+course = canvas.get_course(1714771)
+assignments = course.get_assignments(bucket='unsubmitted')
+
+for assignment in assignments:
+    print(assignment)
