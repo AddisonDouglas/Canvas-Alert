c4fe3725132dd1817083aa0eb8be37a16183a488
Addison Douglas <addisondouglas850@gmail.com>
2024-01-30 12:43:43 -0800

    testing functions

--- Main.py
+++ Main.py
@@ -8,8 +8,16 @@ API_KEY = "10~2fyu2iLVjm1XSaW5uc8QAQf8voBcevOCDQOwQNsQOaCeUw27VpOGic01sNETQWbp"
 
 # Initialize a new Canvas object
 canvas = Canvas(API_URL, API_KEY)
-course = canvas.get_course(1714771)
-assignments = course.get_assignments(bucket='unsubmitted')
+# course = canvas.get_course(1714771)
+# assignments = course.get_assignments(bucket='unsubmitted')
 
-for assignment in assignments:
-    print(assignment)
+# for assignment in assignments:
+#     print(assignment)
+
+
+user = canvas.get_user(4432959)
+courses = user.get_courses()
+for courses in courses:
+    assignments = user.get_assignments(courses);
+    for assignments in assignments:
+        print(assignments)
